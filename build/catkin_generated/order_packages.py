0fdeef0ae1100a9c7be0e064f34a7d34000a3ff3
Caio Amaral <caioaamaral@gmail.com>
2019-02-21 13:44:47 -0300

    add git ignore

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/caioaamaral/vision_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/caioaamaral/vision_ws/devel;/opt/ros/kinetic".split(';') if "/home/caioaamaral/vision_ws/devel;/opt/ros/kinetic" != "" else []
