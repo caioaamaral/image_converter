3f1a3b506844e925ec352184e5e59781c20bc58e
Caio Amaral <caioaamaral@gmail.com>
2019-02-21 12:29:30 -0300

    first commit

---
+++ build/image_converter/CMakeFiles/image_converter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image_converter.dir/src/image_converter.cpp.o"
+  "CMakeFiles/image_converter.dir/src/draw.cpp.o"
+  "/home/caioaamaral/vision_ws/devel/lib/libimage_converter.pdb"
+  "/home/caioaamaral/vision_ws/devel/lib/libimage_converter.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image_converter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/image_converter/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/caioaamaral/vision_ws/src/image_converter/include".split(';') if "/home/caioaamaral/vision_ws/src/image_converter/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "cv_bridge;image_transport;roscpp;sensor_msgs;std_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-limage_converter".split(';') if "-limage_converter" != "" else []
+PROJECT_NAME = "image_converter"
+PROJECT_SPACE_DIR = "/home/caioaamaral/vision_ws/devel"
+PROJECT_VERSION = "0.0.0"
